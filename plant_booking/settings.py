b7f661c55c18ca9119c5804e820726c2e0bab660
Rakesh Kumar T <rakeshptajlapur@gmail.com>
2025-01-18 15:30:17 +0530

    Homepage UI fix, Dynamic welcome msg header & Login page UI fix

--- plant_booking/settings.py
+++ plant_booking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'plantation'
+    'plantation',
 ]
 
 MIDDLEWARE = [
@@ -55,18 +55,21 @@ ROOT_URLCONF = 'plant_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [ ],  # Root-level templates
+        'APP_DIRS': True,  # Allows Django to locate templates inside apps
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages',       
+
             ],
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'plant_booking.wsgi.application'
 
@@ -126,6 +129,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 LOGIN_REDIRECT_URL = '/'  # Redirect to homepage after login
+
 
 
 
