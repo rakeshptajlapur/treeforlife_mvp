c18e1f2d36114e91a288c95759301e37e60f124a
Rakesh Kumar T <rakeshptajlapur@gmail.com>
2025-02-19 01:21:42 +0530

    Updated ALLOWED_HOSTS for PythonAnywhere deployment

--- plant_booking/settings.py
+++ plant_booking/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-x^=&9gnphr-$b1sy5(ouaa56$(zpac3q0=4@)hlo!orvk07^mv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rakeshptajlapur.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
