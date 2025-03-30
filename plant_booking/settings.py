6bb81bfef4f3c1f54e63c24c8161ade13b6cf648
Rakesh Kumar T <rakeshptajlapur@gmail.com>
2025-03-30 12:07:42 +0530

    Updated allowed hosts for subdomain app.treeforlife.net

--- plant_booking/settings.py
+++ plant_booking/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['rakeshptajlapur.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['rakeshptajlapur.pythonanywhere.com', '127.0.0.1', 'localhost', 'app.treeforlife.net']
 
 
 # Application definition
