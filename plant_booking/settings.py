e4b083bdf5f51c3c78fbe545e8038f50f9af2856
Rakesh Kumar T <rakeshptajlapur@gmail.com>
2025-07-15 08:32:08 +0530

    db was changed to mysql accidentaly so now fixed it back to postgres

--- plant_booking/settings.py
+++ plant_booking/settings.py
@@ -97,15 +97,12 @@ WSGI_APPLICATION = 'plant_booking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
     }
 }
 
